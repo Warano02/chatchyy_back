9d9b008741853b41d1b9f2c9c7764d417837771b
Warano02 <carineteoi@gmail.com>
2026-03-31 00:28:33 +0100

    fix: cookie problem

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
@@ -7,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none", 
-    secure: true,     
+    secure: true,    
   });
 
   return token;
